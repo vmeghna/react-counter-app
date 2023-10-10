f55b6fc28a6b9b73f11901c9d134c25328ee0274
vmeghna <shrimeghna97@gmail.com>
2023-10-10 08:29:41 +0530

    only ui done react has to be done

---
+++ components/app.js
@@ -0,0 +1,30 @@
+// import React, { Component } from "react";
+
+
+// class App extends Component {
+//   state = {
+//     counters: [
+//       { id: 1, value: 0 },
+//       { id: 2, value: 0 },
+//       { id: 3, value: 0 },
+//       { id: 4, value: 0 },
+//     ],
+//   };
+
+//   handleIncrement = (counter) => {
+//     const counters = [...this.state.counters];
+//     const index = counters.indexOf(counter);
+//     counters[index] = { ...counters[index] };
+//     counters[index].value++;
+//     this.setState({ counters });
+//   };
+
+  
+
+//   render() {
+      
+       
+//   }
+// }
+
+// export default App;
---
+++ components/index.js
@@ -0,0 +1,8 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./app";
+// import "tailwind/dist/css/tailwind.css";
+// import "font-awesome/css/font-awesome.css";
+
+// ReactDOM.render(<App />, document.getElementById("root"));
